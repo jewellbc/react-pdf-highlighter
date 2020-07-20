a85a23380491dc282e44a9a69e2eeff5bf767d94
jewellbc <i7j@ornl.gov>
2020-07-20 01:07:29 -0400

    removed emojis from tip popup

--- packages/react-pdf-highlighter/src/components/Tip.js
+++ packages/react-pdf-highlighter/src/components/Tip.js
@@ -69,22 +69,7 @@ class Tip extends Component<Props, State> {
                   }
                 }}
               />
-              <div>
-                {["💩", "😱", "😍", "🔥", "😳", "⚠️"].map(_emoji => (
-                  <label key={_emoji}>
-                    <input
-                      checked={emoji === _emoji}
-                      type="radio"
-                      name="emoji"
-                      value={_emoji}
-                      onChange={event =>
-                        this.setState({ emoji: event.target.value })
-                      }
-                    />
-                    {_emoji}
-                  </label>
-                ))}
-              </div>
+
             </div>
             <div>
               <input type="submit" value="Save" />
